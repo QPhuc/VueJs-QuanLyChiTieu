af1d6f1e452171a0fc3bcc9dc099f70a35310703
Qphuc <tqp1610@gmail.com>
2021-08-08 12:02:06 +0700

    search text

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -2,14 +2,36 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <h1>This is an Home page</h1>
+    <input type="text" v-model="searchText">
+    <ul>
+      <li v-for="(customer, index) in customersFilled" :key="index">
+        {{ customer }}
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-
+import { ref, reactive, computed } from 'vue';
 export default {
   name: 'Home',
+  setup() {
+    const searchText = ref("");
+    const customers = reactive (["SomeThing", "Phuc Tran", "Quang Phuc"])
+    const customersFilled = computed(() => 
+      customers
+      .map(customer => {
+        customer = customer.toLowerCase();
+        return customer;
+      })
+      .filter(customer => customer.includes(searchText.value.toLowerCase()))
+    )
+
+    return {
+      searchText,
+      customersFilled,
+    }
+  },
   components: {
     
   }
