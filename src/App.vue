2fb2edd186c75c3299ce24a530a3a68b4ee9a2f6
Qphuc <tqp1610@gmail.com>
2021-08-08 23:59:23 +0700

    connect firebase

--- src/App.vue
+++ src/App.vue
@@ -7,11 +7,24 @@
 
 <script>
 import Nav from './components/Nav.vue'
+import { fireStoreCore } from './configs/firebase'
 export default {
   name: 'App',
   components: {
     Nav
+  },
+  setup() {
+    async function getTransactions() {
+      const response = await fireStoreCore.collection('transactions').get();
+      console.log(response.docs);
+      const data = response.docs.map(doc => {
+        return {...doc.data(), id: doc.id }
+      })
+      console.log(data);
+    }
+    getTransactions()
   }
+  
 }
 </script>
 
---
+++ src/configs/firebase.js
@@ -0,0 +1,19 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCzZaj2B6YUoRoAV9YExOgYfP3uK-UfOR4",
+    authDomain: "quan-ly-chi-tieu-d0a28.firebaseapp.com",
+    projectId: "quan-ly-chi-tieu-d0a28",
+    storageBucket: "quan-ly-chi-tieu-d0a28.appspot.com",
+    messagingSenderId: "771929421818",
+    appId: "1:771929421818:web:5783c03c615dfd48215643",
+    measurementId: "G-CRZW1M8RFK"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const fireStoreCore = firebase.firestore();
+
+export { fireStoreCore };
