71095eebb053387e01b9443052e9df252384d314
Qphuc <tqp1610@gmail.com>
2021-08-09 19:58:24 +0700

    fix bug

--- src/main.js
+++ src/main.js
@@ -1,8 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import store from './store/index'
-import diy from './diy'
+
 import './assets/styles/tailwind.css'
 import './assets/styles/global.css'
 import { registerGlobalComponents } from './utils/import'
@@ -11,9 +10,6 @@ const app = createApp(App);
 
 registerGlobalComponents(app)
 
-app.provide("diy", diy);
-
 app.use(router);
-app.use(store);
 
 app.mount('#app')
--- src/router/index.js
+++ src/router/index.js
@@ -1,30 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
-import Transactions from '../views/Transactions.vue'
-import TransactionsDetail from '../views/Transactions-detail.vue'
 import NotFound from '../views/404.vue'
 
 const routes = [
   {
     path: '/',
-    component: Home,
-  },
-  {
-    path: '/transactions',
-    name: 'transaction-route',
-    component: Transactions,
-    meta: {
-      layout: 'auth'
-    }
-  },
-  {
-    path: '/transactions/:id',
-    name: 'transaction-details-route',
-    component: TransactionsDetail,
-  },
-  {
-    path: '/ts',
-    redirect: '/transactions'
+    name: 'Home',
+    component: () => import (/* webpackChunkName: "home" */ "../views/index.vue"),
   },
   {
     path: '/:pathMatch(.*)*',
