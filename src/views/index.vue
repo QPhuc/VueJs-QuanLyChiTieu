eb5cc0a9dfe5bebe0ca274e29d6b80548df1f022
Qphuc <tqp1610@gmail.com>
2021-08-10 10:16:18 +0700

    show user

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,11 +1,18 @@
 <template>
   <div>
-    
+    <div class="mt-6" v-if="user">{{ user.displayName }}</div>
   </div>
 </template>
 
 <script>
+  import { useUser } from '@/composables/useUser'
 export default {
   name: 'Home',
+  setup() {
+    const { getUser } = useUser();
+    const { user } = getUser();
+
+    return { user };
+  }
 }
 </script>
